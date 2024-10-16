1147cae0a5805be944a40cd08f45e786705b2242
drmcarvalho <furiano.k@gmail.com>
2024-10-16 14:10:23 -0300

    refac calc

--- TesteCaixa.Api/ServiceDomain/BoxCalculateDimensionService.cs
+++ TesteCaixa.Api/ServiceDomain/BoxCalculateDimensionService.cs
@@ -17,13 +17,19 @@ namespace TesteCaixa.Api.ServiceDomain
             var ordersCalculated = new OrdersCalculatedDto();            
             foreach (var order in dto.Orders) if (order.Products.Count > 0)
             {
-                var orderCalculated = new OrderProcessedDto { OrderId = order.OrderId };
+                var orderCalculated = new OrderProcessedDto { OrderId = order.OrderId };                
                 foreach (var box in boxes)
                 {
                     var boxDto = new BoxDto { BoxId = box.BoxId };                    
                     var volumeBox = Volume(box.Length, box.Width, box.Height);
+                    var minDimensionBox = GetMinDimension(box.Length, box.Height, box.Width);
                     foreach (var product in order.Products.ToList()) if (DimensionsIsValid(product.Dimensions!))
                     {                        
+                        var maxDimensionProduct = GetMaxDimension(product.Dimensions!.Length, product.Dimensions.Height, product.Dimensions.Width);
+                        if (maxDimensionProduct > minDimensionBox)
+                        {
+                            continue;
+                        }
                         var volumeProduct = Volume(product.Dimensions!.Length, product.Dimensions.Width, product.Dimensions.Height);
                         if (volumeProduct > volumeBox)
                         {
@@ -42,7 +48,33 @@ namespace TesteCaixa.Api.ServiceDomain
             return ordersCalculated;            
         }
 
-        private bool DimensionsIsValid(DimensionsDto dimensions) 
+        private static bool DimensionsIsValid(DimensionsDto dimensions) 
             => dimensions is not null && dimensions.Width > 0 && dimensions.Length > 0 && dimensions.Height > 0;
+
+        private static int GetMaxDimension(int length, int height, int width)
+        {
+            if ((length > height) && (length > width))
+            {
+                return length;
+            }
+            else if ((height > length) && (height > width))
+            {
+                return height;
+            }
+            return width;
+        }
+
+        private static int GetMinDimension(int length, int height, int width)
+        {
+            if ((length < height) && (length < width))
+            {
+                return length;
+            }
+            else if ((height < length) && (height < width))
+            {
+                return height;
+            }
+            return width;
+        }
     }
 }
